2ce46880beffc24789b943fc2a28d7225ad30d8f
unknown <ritikasinghal24062001@gmail.com>
2020-09-02 16:56:17 +0530

    second commit

---
+++ ASSIGNMENT2/Q2range.py
@@ -0,0 +1,10 @@
+def val():
+    n=int(input("enter your number"))
+    a=int(input("input  lower range"))
+    b=int(input("input  higher range"))
+    if(a<=n and b>=n):
+
+       print("number is in given range")
+    else:
+        print("number is not in range")
+val()
